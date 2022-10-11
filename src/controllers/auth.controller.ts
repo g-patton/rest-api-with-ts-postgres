8ca9dfb2ec48aadf019edeebbc5950edf14acf70
Graham Patton <graham.m.patton@gmail.com>
2022-10-11 18:13:11 -0400

    logout, refresh token, login added

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -25,15 +25,15 @@ const cookiesOptions: CookieOptions = {
       Date.now() + config.get<number>('accessTokenExpiresIn') * 60 * 1000
     ),
     maxAge: config.get<number>('accessTokenExpiresIn') * 60 * 1000,
-  };
+};
   
-  const refreshTokenCookieOptions: CookieOptions = {
+const refreshTokenCookieOptions: CookieOptions = {
     ...cookiesOptions,
     expires: new Date(
       Date.now() + config.get<number>('refreshTokenExpiresIn') * 60 * 1000
     ),
     maxAge: config.get<number>('refreshTokenExpiresIn') * 60 * 1000,
-  };
+};
   
   
 export const registerUserHandler = async (
@@ -58,6 +58,7 @@ export const registerUserHandler = async (
       });
     } catch (err: any) {
       if (err.code === '23505') {
+        // made up error code
         return res.status(409).json({
           status: 'fail',
           message: 'User with that email already exist',
@@ -65,5 +66,127 @@ export const registerUserHandler = async (
       }
       next(err);
     }
+};
+  
+  
+export const loginUserHandler = async (
+    req: Request<{}, {}, LoginUserInput>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const { email, password } = req.body;
+      const user = await findUserByEmail({ email });
+  
+      //1. Check if user exists and password is valid
+      if (!user || !(await User.comparePasswords(password, user.password))) {
+        return next(new AppError(400, 'Invalid email or password'));
+      }
+  
+      // 2. Sign Access and Refresh Tokens
+      const { access_token, refresh_token } = await signTokens(user);
+  
+      // 3. Add Cookies
+      res.cookie('access_token', access_token, accessTokenCookieOptions);
+      res.cookie('refresh_token', refresh_token, refreshTokenCookieOptions);
+      res.cookie('logged_in', true, {
+        ...accessTokenCookieOptions,
+        httpOnly: false,
+      });
+  
+      // 4. Send response
+      res.status(200).json({
+        status: 'success',
+        access_token,
+      });
+    } catch (err: any) {
+      next(err);
+    }
+};
+  
+
+export const refreshAccessTokenHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const refresh_token = req.cookies.refresh_token;
+
+    const message = 'Could not refresh access token';
+
+    if (!refresh_token) {
+      return next(new AppError(403, message));
+    }
+
+    // Validate refresh token
+    const decoded = verifyJwt<{ sub: string }>(
+      refresh_token,
+      'refreshTokenPublicKey'
+    );
+
+    if (!decoded) {
+      return next(new AppError(403, message));
+    }
+
+    // Check if user has a valid session
+    const session = await redisClient.get(decoded.sub);
+
+    if (!session) {
+      return next(new AppError(403, message));
+    }
+
+    // Check if user still exist
+    const user = await findUserById(JSON.parse(session).id);
+
+    if (!user) {
+      return next(new AppError(403, message));
+    }
+
+    // Sign new access token
+    const access_token = signJwt({ sub: user.id }, 'accessTokenPrivateKey', {
+      expiresIn: `${config.get<number>('accessTokenExpiresIn')}m`,
+    });
+
+    // 4. Add Cookies
+    res.cookie('access_token', access_token, accessTokenCookieOptions);
+    res.cookie('logged_in', true, {
+      ...accessTokenCookieOptions,
+      httpOnly: false,
+    });
+
+    // 5. Send response
+    res.status(200).json({
+      status: 'success',
+      access_token,
+    });
+  } catch (err: any) {
+    next(err);
+  }
+};
+
+// no direct way to clear cookies, so send empty string cookie with max age-1
+const logout = (res: Response) => {
+    res.cookie('access_token', '', { maxAge: -1 });
+    res.cookie('refresh_token', '', { maxAge: -1 });
+    res.cookie('logged_in', '', { maxAge: -1 });
   };
   
+  export const logoutHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const user = res.locals.user;
+  
+      await redisClient.del(user.id);
+      logout(res);
+  
+      res.status(200).json({
+        status: 'success',
+      });
+    } catch (err: any) {
+      next(err);
+    }
+};
