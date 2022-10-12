4684e03871645879f946c36f20c93085d7c93b52
Graham Patton <graham.m.patton@gmail.com>
2022-10-12 11:33:46 -0400

    routes added

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import {
+  loginUserHandler,
+  logoutHandler,
+  refreshAccessTokenHandler,
+  registerUserHandler,
+} from '../controllers/auth.controller';
+import { deserializeUser } from '../middleware/deserializeUser';
+import { requireUser } from '../middleware/requireUser';
+import { validate } from '../middleware/validate';
+import { createUserSchema, loginUserSchema } from '../schemas/user.schema';
+
+const router = express.Router();
+
+// Register user
+router.post('/register', validate(createUserSchema), registerUserHandler);
+
+// Login user
+router.post('/login', validate(loginUserSchema), loginUserHandler);
+
+// Logout user
+router.get('/logout', deserializeUser, requireUser, logoutHandler);
+
+// Refresh access token
+router.get('/refresh', refreshAccessTokenHandler);
+
+export default router;
